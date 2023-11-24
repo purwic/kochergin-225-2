ccad0d39f247b3abedacae7373d317d706f78529
purwic <rodion.kochergin@gmail.com>
2023-11-24 13:06:16 +0300

    3 complited

--- 3/MainWindow.xaml.cs
+++ 3/MainWindow.xaml.cs
@@ -24,5 +24,75 @@ namespace _3
         {
             InitializeComponent();
         }
+
+        /*
+        
+        1 янв - 1 день года
+        1 день года - четверг, тоесть имеет номер 4
+        2 день года - 5
+        3 день - 6
+        4 день - 0
+        и т.д.
+
+        в терминах функции f сопостовления номера дня к дню в аргументе:
+
+        f(1) = 4
+        f(2) = 5
+        f(3) = 6
+        f(4) = 0
+        f(5) = 1
+        f(6) = 2
+        f(7) = 3
+        f(8) = f(1) = 4
+        ...
+
+        тоесть функция сопостовления номера к K-ому дню будет:
+        
+        f(k) = (k + 3) mod 7
+        
+        */
+        private int f(int n)
+        {
+            return (n + 3) % 7;
+        }
+
+
+
+        private void K_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+
+                int k = int.Parse(K.Text);
+
+                if (k < 1 || k > 365)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                DayOfWeek.Content = $"{f(k)}";
+
+            }
+
+            catch (FormatException)
+            {
+                DayOfWeek.Content = "-";
+            }
+
+            catch (ArgumentOutOfRangeException)
+            {
+                DayOfWeek.Content = "K вне возм. знач.";
+            }
+
+            catch
+            {
+                DayOfWeek.Content = "-";
+            }
+
+
+
+        }
+
     }
+
 }
